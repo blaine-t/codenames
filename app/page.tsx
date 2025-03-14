ce9119ef08bc6b87ea62a200eb542b609c920c6b
blaine-t <blaine-t@bathost.net>
2025-03-14 11:01:16 -0500

    Actually add cool background for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Home() {
   return (
-    <>
-      <Hero />
-      <h1 style={{textAlign: 'center'}}>Sign up or sign in now for some Codenames fun!</h1>
-    </>
+    <div className="background">
+      <div className="wrapper">
+        <Hero />
+        <h1 style={{textAlign: 'center'}}>Sign up or sign in now for some Codenames fun!</h1>
+      </div>
+    </div>
   );
 }
