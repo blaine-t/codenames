93c2c46314b31b65ee822ed4cbe18f942e512a8d
Blaine Traudt <blaine-t@bathost.net>
2025-04-21 14:51:40 -0500

    Add example home client test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/hero";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="background">
       <div className="wrapper">
