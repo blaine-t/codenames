6e441880ffe956c30edd1bb5b7e2d7a35d9e59eb
blaine-t <blaine-t@bathost.net>
2025-04-29 17:23:53 -0500

    Change how the board works a little bit

--- app/api/createGame/route.ts
+++ app/api/createGame/route.ts
@@ -3,13 +3,6 @@ import { createClient } from "@/utils/supabase/server";
 
 import { generate } from "random-words"
 
-enum Allegiance {
-    Blue = "blue",
-    Red = "red",
-    Bystander = "bystander",
-    Assassin = "assassin"
-}
-
 // https://stackoverflow.com/a/2450976
 function shuffle(array: any[]) {
     let currentIndex = array.length;
@@ -27,20 +20,25 @@ function shuffle(array: any[]) {
     }
 }
 
-function generateBoard() {
+function generateBoard(team1_id: number, team2_id: number) {
     const words = generate({ minLength: 3, maxLength: 7, exactly: 25 }) as string[]
-    const allegiances = [
-        ...Array(8).fill(Allegiance.Blue),
-        ...Array(7).fill(Allegiance.Red),
-        ...Array(9).fill(Allegiance.Bystander),
-        ...Array(1).fill(Allegiance.Assassin)
+    const team_ids = [
+        ...Array(8).fill(team1_id),
+        ...Array(7).fill(team2_id),
+        // Bystander
+        ...Array(9).fill(0),
+        // Assassin
+        ...Array(1).fill(-1)
     ]
-    shuffle(allegiances)
+    shuffle(team_ids)
 
     const board = words.map((word, i) => {
         return {
             word,
-            allegiance: allegiances[i],
+            // Give team_id if is real team otherwise parse different
+            team_id: team_ids[i] > 0 ? team_ids[i] : null,
+            is_bystander: team_ids[i] === 0,
+            is_assassin: team_ids[i] === -1,
             guessed: false
         }
     })
@@ -54,7 +52,7 @@ export async function POST(req: Request) {
     // Get data from the client
     const { game_code, team1_id, team2_id, turn_time } = await req.json() as CreateGameJson
 
-    const board = generateBoard()
+    const board = generateBoard(team1_id, team2_id)
 
     const { error: upsertError } = await supabase.from("Game").upsert({
         team1_id,
