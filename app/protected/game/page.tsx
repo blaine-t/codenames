3e9c58503ee9eb165259f066da7031c60a6ab552
blaine-t <blaine-t@bathost.net>
2025-04-30 01:57:55 -0500

    Add support for Spymaster to give clue

--- app/protected/game/page.tsx
+++ app/protected/game/page.tsx
@@ -22,8 +22,22 @@ function GameContent() {
   const [turnTime, setTurnTime] = useState<number | null>(null);
   const [board, setBoard] = useState<Board[] | null>(null);
   const [clue, setClue] = useState<Clue | undefined>(undefined);
+  const [isGuesser, setIsGuesser] = useState<boolean>(true);
+  const [playerId, setPlayerId] = useState<number | null>(null);
+  const [selectedPlayerId, setSelectedPlayerId] = useState<number | null>(null);
 
   useEffect(() => {
+    const fetchGameData = async (playerIdCurrent: number) => {
+      const { data: gameData, error: gameError } = await supabase
+        .from("Game")
+        .select("selected_player_id, board, turn_time")
+        .eq("game_code", gameCode)
+        .single();
+      setSelectedPlayerId(gameData?.selected_player_id)
+      setBoard(gameData?.board);
+      setTurnTime(gameData?.turn_time);
+    };
+
     const fetchPlayerData = async () => {
       const {
         data: { user: authUser },
@@ -48,7 +62,7 @@ function GameContent() {
 
       const { data: playerData, error: playerError } = await supabase
         .from("Player")
-        .select("team_id, is_guesser")
+        .select("id, team_id, is_guesser")
         .eq("user_id", userData.id)
         .order("created_at", { ascending: false })
         .single();
@@ -74,59 +88,51 @@ function GameContent() {
         : "Spymaster";
       const readableTeam = teamData.name;
 
-      setRole(readableRole);
-      setTeam(readableTeam);
+      setPlayerId(playerData.id)
+      setIsGuesser(playerData.is_guesser)
+      setRole(readableRole)
+      setTeam(readableTeam)
       setTeamId(playerData.team_id)
+      await fetchGameData(playerData.id);
     };
 
     fetchPlayerData();
   }, []);
-
-  useEffect(() => {
-    const fetchGameData = async () => {
-      const { data: gameData, error: gameError } = await supabase
-        .from("Game")
-        .select("board, turn_time")
-        .eq("game_code", gameCode)
-        .single();
-
-      setBoard(gameData?.board);
-      setTurnTime(gameData?.turn_time);
-    };
-    fetchGameData();
-  }, []);
-
   // Enable netcode for handling when game state changes
-    useEffect(() => {
-      supabase
-        .channel("schema-db-changes")
-        .on(
-          "postgres_changes",
-          {
-            event: "*",
-            schema: "public",
-            table: "Game",
-            filter: `game_code=eq.${gameCode}`,
-          },
-          async (payload) => {
-            if (payload.new && 'board' in payload.new) {
-              setBoard(payload.new.board as Board[])
-            }
+  useEffect(() => {
+    supabase
+      .channel("schema-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "Game",
+          filter: `game_code=eq.${gameCode}`,
+        },
+        async (payload) => {
+          if (payload.new && 'board' in payload.new) {
+            setBoard(payload.new.board as Board[])
+          }
 
-            if (payload.new && 'clue_id' in payload.new && payload.new.clue_id) {
-              const { data: clueRecord } = await supabase
+          if (payload.new && 'clue_id' in payload.new && payload.new.clue_id) {
+            const { data: clueRecord } = await supabase
               .from("Clue")
               .select('phrase, count, remaining_guesses')
               .eq("id", payload.new.clue_id)
               .single()
-              setClue(clueRecord as Clue)
-            } else {
-              setClue(undefined)
-            }
+            setClue(clueRecord as Clue)
+          } else {
+            setClue(undefined)
           }
-        )
-        .subscribe();
-    }, []);
+
+          if (payload.new && 'selected_player_id' in payload.new) {
+            setSelectedPlayerId(payload.new.selected_player_id)
+          }
+        }
+      )
+      .subscribe();
+  }, []);
 
   function handleClick(id: number) {
     const sendGuess = async () => {
@@ -144,13 +150,32 @@ function GameContent() {
     sendGuess()
   }
 
+  function submitClue(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const sendClue = async () => {
+      const form = event.currentTarget as HTMLFormElement;
+      const data = {
+        game_code: gameCode,
+        player_id: playerId,
+        phrase: (form.elements.namedItem('phrase') as HTMLInputElement).value,
+        count: parseInt((form.elements.namedItem('count') as HTMLInputElement).value)
+      }
+      await fetch("/api/submitClue", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify(data)
+      })
+    }
+    sendClue()
+  }
+
   return (
     <>
       <TimeBox seconds={turnTime} />
       <div className="table">
         <RoleBox role={`${role} (${team})`} />
-        <CardGrid isGuesser={role === 'Field Operative'} board={board} handleClick={handleClick} />
-        <StatusBox clue={clue} />
+        <CardGrid isGuesser={isGuesser} board={board} handleClick={handleClick} />
+        <StatusBox clue={clue} isGuesser={isGuesser} needClue={selectedPlayerId === playerId && !isGuesser} submitClue={submitClue} />
       </div>
     </>
   );
--- components/game/statusBox.tsx
+++ components/game/statusBox.tsx
@@ -3,16 +3,30 @@ import styles from './StatusBox.module.css';
 
 interface RoleBoxProps {
   clue?: Clue;
+  isGuesser: boolean;
+  needClue: boolean;
+  submitClue: any;
 }
 
-export default function StatusBox({ clue }: RoleBoxProps) {
-
+export default function StatusBox({ clue, isGuesser, needClue, submitClue }: RoleBoxProps) {
   return (
     <div className={styles.box}>
       {(clue) ? <>
         <h1>HINT: {clue.phrase.toUpperCase()} - {clue.count}</h1>
         <h1>GUESSES LEFT - {clue.remaining_guesses}</h1>
-      </> : <h1>Waiting for clue...</h1>
+      </> : 
+      (!isGuesser && needClue) ? <form onSubmit={submitClue} className={styles.inputContainer}>
+        <div className={styles.wrapper}>
+        <label className={styles.label}>Hint</label>
+        <input type='text' className={styles.textInput} name='phrase'></input>
+        </div>
+        <div className={styles.wrapper}>
+        <label className={styles.label}>Count</label>
+        <input className={styles.numberInput} type='number' name='count'></input>
+        </div>
+        <button type='submit'>Submit</button>
+      </form> : 
+      <h1>Waiting for clue...</h1>
       }
     </div>
   );
